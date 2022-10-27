87bf4cc29900b70bdbb4bf2750b328d07c3c3ce5
brokensito <dsanzfuertes.15@gmail.com>
2022-10-27 19:21:10 +0200

    experimentacion carlos

---
+++ ej3.py
@@ -0,0 +1,17 @@
+class Alumno:
+    
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+
+    def __str__(self):
+        return "El alumno se ha creado con exito."
+
+    def calificacion(self):
+        if self.nota>=5:
+            mensaje = "El alumno ha APROBADO con una nota de: {}".format(self.nota)
+
+        else:
+            mensaje= "El alumno ha SUSPENDIDO con una nota de: {}".format(self.nota)
+
+        return mensaje
