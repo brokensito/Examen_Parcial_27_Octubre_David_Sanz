b48b2db1cfd465ded6fe6588210bb019dce8b9fc
brokensito <dsanzfuertes.15@gmail.com>
2022-10-27 20:07:34 +0200

    convertir 4

--- ej4.py
+++ ej4.py
@@ -1,1 +1,9 @@
-from algoritmos.hash import 
+from algoritmos.hash import crear_tabla, funcion_hash, cantidad_elementos, agregar, buscar, quitar
+
+
+
+def convertir8 (valor: str) -> str:
+    if not valor.isascii():
+        raise ValueError("No se pueden introducir ASCII")
+    return "".join(f"{ord(i):08b}" for i in valor)
+
