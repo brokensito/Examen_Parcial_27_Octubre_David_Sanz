0aa4929bafa90cf3fb5d2d92cfc5ab87244b9e9f
brokensito <dsanzfuertes.15@gmail.com>
2022-10-27 19:17:59 +0200

    ej 2 commit

---
+++ ej1.py
@@ -0,0 +1,12 @@
+lista = [18, 50, 210, 80, 145, 333, 70, 30]
+
+def ejercicio1(lista):
+    for i in range(len(lista)):
+        if lista[i]%10 == int and lista[i]<200:
+            print(i)
+
+ejercicio1(lista)
+
+
+
+    
